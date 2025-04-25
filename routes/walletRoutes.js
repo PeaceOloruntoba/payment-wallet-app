8e9a1186dd78ddbab197a10bb76683459feae018
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 00:36:13 +0100

    Create walletRoutes.js

---
+++ routes/walletRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  getUserWallet,
+  createWallet,
+  transferFunds,
+  createVirtualCard,
+  getUserVirtualCard,
+} = require("../controllers/walletController");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.get("/my-wallet", protect, getUserWallet);
+router.post("/", protect, createWallet);
+router.post("/transfer", protect, transferFunds);
+router.post("/card", protect, createVirtualCard);
+router.get("/card", protect, getUserVirtualCard);
+
+module.exports = router;
