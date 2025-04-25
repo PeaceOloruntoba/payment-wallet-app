e933d36eb3ca7771968edae6eb70f5d4771ff2af
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 00:26:46 +0100

    Create VirtualCard.js

---
+++ models/VirtualCard.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const virtualCardSchema = new mongoose.Schema({
+  walletId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Wallet",
+    required: true,
+    unique: true,
+  },
+  cardNumber: { type: String, required: true, unique: true },
+  expiryDate: { type: String, required: true },
+  cvv: { type: String, required: true },
+});
+
+const VirtualCard = mongoose.model("VirtualCard", virtualCardSchema);
+module.exports = VirtualCard;
