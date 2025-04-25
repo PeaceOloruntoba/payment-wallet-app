63f079e7c5ef944fdc3afe7afff22c39aba5a6ff
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 00:39:28 +0100

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,78 @@
+const User = require('../models/User');
+const Wallet = require('../models/Wallet');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// Function to generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, 'your-secret-key', {
+    expiresIn: '30d',
+  });
+};
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+     //create wallet for user
+     const wallet = await Wallet.create({
+         userId: user._id,
+     })
+     user.walletId = wallet._id;
+     await user.save()
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+        walletId: wallet._id
+      });
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `Error: ${error.message}` });
+  }
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email }).populate('walletId');
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id),
+                wallet: user.walletId,
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: `Error: ${error.message}` });
+    }
+};
+
+const getUserProfile = async (req, res) => {
+  res.json(req.user);
+};
+
+module.exports = { registerUser, loginUser, getUserProfile };
