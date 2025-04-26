ed85812e2d9eeab21672d243ba7c3e2d5524e6ea
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 15:12:41 +0100

    Update walletRoutes.js

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -6,11 +6,13 @@ import {
   transfer,
 } from "../controllers/walletController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import { startDeposit } from "../controllers/walletController.js";
 
 const router = express.Router();
 
 router.get("/balance", protect, getBalance);
 router.post("/deposit", protect, deposit);
+router.post("/deposit/start", protect, startDeposit);
 router.post("/withdraw", protect, withdraw);
 router.post("/transfer", protect, transfer);
 
