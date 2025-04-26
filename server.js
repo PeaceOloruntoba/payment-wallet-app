c2d61a0a761f7299dd677d679952ba879dbed8ed
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 14:38:34 +0100

    asd

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-import userRoutes from "./routes/userRoutes.js";
-import walletRoutes from "./routes/walletRoutes.js";
+import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./config/db.js";
-import express from "express";
-import cors from "cors";
 
-const app = express();
-
-// Connect to MongoDB
-connectDB();
-
-// Middleware
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse JSON request bodies
-
-// Routes
-app.use("/api/users", userRoutes);
-app.use("/api/wallets", walletRoutes);
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
