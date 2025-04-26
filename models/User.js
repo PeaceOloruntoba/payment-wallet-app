cc27d7cfb8ba1dba7026b2d24db2c4adfa6797c8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-26 16:29:21 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
-  import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-  const userSchema = new mongoose.Schema(
-    {
-      name: { type: String, required: true },
-      email: { type: String, required: true, unique: true },
-      phone: { type: String },
-      password: { type: String, required: true },
-      currency: { type: String, required: true },
-      walletBalance: { type: Number, default: 0 },
-      accountNumber: { type: String, unique: true },
-    },
-    { timestamps: true }
-  );
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String },
+    password: { type: String, required: true },
+    currency: { type: String, required: true },
+    walletBalance: { type: Number, default: 0 },
+    accountNumber: { type: String, unique: true },
+    virtualCardId: { type: String },
+  },
+  { timestamps: true }
+);
 
-  const User = mongoose.model("User", userSchema);
-  export default User;
+const User = mongoose.model("User", userSchema);
+export default User;
